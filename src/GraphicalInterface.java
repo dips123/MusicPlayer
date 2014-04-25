94dc8caed2b41e6884726138912e4cd3f2303ffb
dips123 <dipatil15@gmail.com>
2014-04-25 10:25:24 +0530

    Update GraphicalInterface.java

--- src/GraphicalInterface.java
+++ src/GraphicalInterface.java
@@ -3,10 +3,12 @@
  * Author : Karthik Abinav S ; CS10B057
  */
 
-import java.awt.Dimension;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.util.List;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class GraphicalInterface {
 	static JFrame window;
@@ -15,14 +17,10 @@ public class GraphicalInterface {
 	static JButton stop;
 	static JButton pause;
 	static JButton open;
+	static JSlider js;
+	static JLabel name;
 
-	/*
-	 * Main method to initiate the GUI
-	 * 
-	 * @params : String []
-	 * 
-	 * @return : void
-	 */
+	
 	public static void main(String args[]) {
 		GUI();
 	}
@@ -37,16 +35,25 @@ public class GraphicalInterface {
 	public static void createButtons() {
 
 		open = new JButton();
-		open.setText("Open");
+		open.setText("Select Song");
 		Listener.openListener();
 		player.add(open);
+		open.setToolTipText("Click to brows file");
+		
+		pause = new JButton();
+		pause.setText("Pause");
+		Listener.pauseListener();
+		player.add(pause);
+		pause.setToolTipText("pause");
 
 		stop = new JButton();
 		stop.setText("Stop");
 		Listener.stopListener();
 		player.add(stop);
+		stop.setToolTipText("stop");
+		
 
-		window.pack();
+		//window.pack();
 	}
 
 	/*
@@ -57,16 +64,56 @@ public class GraphicalInterface {
 	 * @return : void
 	 */
 	public static void GUI() {
+		
+		Dimension dim=Toolkit.getDefaultToolkit().getScreenSize();
 		window = new JFrame("Wave Player");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setMinimumSize(new Dimension(400, 200));
+		window.setSize(dim.width/4-120, dim.height/2);
+		window.setLocation(dim.width/2, dim.height/2-200);
+		window.setIconImage(new ImageIcon("g.jpg").getImage());
+		window.setResizable(false);
 
 		player = new JPanel();
+		player.setBackground(Color.black); 
+		
 		window.add(player);
-		window.pack();
+		
+		ImageIcon img2=new ImageIcon("img.jpg");
+		JLabel lim2=new JLabel(img2); 
+		player.add(lim2);
+		
+		ImageIcon img1=new ImageIcon("1.gif");
+		JLabel lim1=new JLabel(img1);
+		player.add(lim1);
+		
+		ImageIcon img=new ImageIcon("loading.gif");
+		JLabel lim=new JLabel(img);
+		player.add(lim);
+		
+		
+		createButtons();
+		js=new JSlider(0,100);
+		js.setMinorTickSpacing(10);
+		js.setToolTipText("Adjust Volume");
+		js.addChangeListener(new ChangeListener() {
+			
+			public void stateChanged(ChangeEvent e) {
+				// TODO Auto-generated method stub
+				int val=js.getValue();
+			
+						
+				}
+		});
+		player.add(js);
+		 
+	/*	String s=Logic.file.toString();
+		name=new JLabel(s);
+		player.add(name);*/
+		
+		//window.pack();
 		window.setVisible(true);
 
-		createButtons();
+		
 	}
 
 }
